a6b3e0f07e283a80ef3ad9a648e95a7526eae59a
BennyG117 <bengomezp@gmail.com>
2023-09-30 16:04:06 -0700

    Update Commit - about me section

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -15,21 +15,11 @@ const Dashboard = () => {
             {/* //TODO: Reminder to insert image logo of name here */}
             {/* <img src="images/logo.png" alt="logo" /> */}
             <ul>
-              <li>
-                <a href="#">Home</a>
-              </li>
-              <li>
-                <a href="#">About</a>
-              </li>
-              <li>
-                <a href="#">Services</a>
-              </li>
-              <li>
-                <a href="#">Portfolio</a>
-              </li>
-              <li>
-                <a href="#">Contact</a>
-              </li>
+              <li> <a href="#">Home</a> </li>
+              <li> <a href="#">About</a> </li>
+              <li> <a href="#">Services</a> </li>
+              <li> <a href="#">Portfolio</a> </li>
+              <li> <a href="#">Contact</a> </li>
             </ul>
           </nav>
           <div className="header-text">
@@ -47,18 +37,33 @@ const Dashboard = () => {
             <div className="row">
                 {/* left for image */}
                 <div className="about-col-1">
-                    {/* //TODO: IMAGE TO GO HERE */}
+                    {/* //TODO: IMAGE TO GO HERE - found work around*/}
                     {/* <img src="../images/bg1small.jpg" alt="" /> */}
                     <img src={bg1small} alt="picture of Ben" />
                 </div>
                 {/* right for text */}
                 <div className="about-col-2">
-                    <h1>About Me</h1>
+                    <h1 className="sub-title">About Me</h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloribus officiis atque saepe facere consequuntur dignissimos corrupti corporis maiores praesentium deserunt, iste animi vitae! Unde dignissimos expedita eius rem ad facere.</p> 
+
+                    <div className="tab-titles">
+                        <p className="tab-links active-link">Skills</p>
+                        <p className="tab-links">Experience</p>
+                        <p className="tab-links">Education</p>
+                    </div>
+                    <div className="tab-contents">
+                        <ul>
+                            {/* //TODO: update written fields and text */}
+                            <li><span>UI/UX</span><br /> Designing Web/App Interfaces</li>
+                            <li><span>Web Development</span><br /> Web app Development</li>
+                            <li><span>App Development</span><br /> Building Android/iOS apps</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
       </div>
-    </div>
+    </div> 
   );
 };
 
