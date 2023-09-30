9390ee138d5bad5684e38a61cb8ed6d3d6fed44a
BennyG117 <bengomezp@gmail.com>
2023-09-29 18:10:07 -0700

    Update Commit - About Me

--- client/src/views/Dashboard.jsx
+++ client/src/views/Dashboard.jsx
@@ -1,33 +1,65 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
+import bg1small from "../images/bg1small.jpg";
 
 //Body view of the page
 const Dashboard = () => {
   return (
     <div>
-    <div id="header">
+      <div id="header">
         <div className="container">
-            <nav>
-                <h1>Ben.</h1>
-                {/* //TODO: Reminder to insert image logo of name here */}
-                {/* <img src="images/logo.png" alt="logo" /> */}
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Portfolio</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+          {/* ------------- NAV BAR & PAGE TITLE -------------  */}
+          <nav>
+            <h2 className="logo">Ben.</h2>
+            {/* //TODO: Reminder to insert image logo of name here */}
+            {/* <img src="images/logo.png" alt="logo" /> */}
+            <ul>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">Services</a>
+              </li>
+              <li>
+                <a href="#">Portfolio</a>
+              </li>
+              <li>
+                <a href="#">Contact</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="header-text">
+            <p>UI/UX Designer</p>
+            <h1>
+              Hi, I'm <span>Ben Gomez </span>
+              <br /> from ...
+            </h1>
+          </div>
         </div>
+      </div>
+      {/* ------------- ABOUT SECTION -------------  */}
+      <div id="about">
+        <div className="container">
+            <div className="row">
+                {/* left for image */}
+                <div className="about-col-1">
+                    {/* //TODO: IMAGE TO GO HERE */}
+                    {/* <img src="../images/bg1small.jpg" alt="" /> */}
+                    <img src={bg1small} alt="picture of Ben" />
+                </div>
+                {/* right for text */}
+                <div className="about-col-2">
+                    <h1>About Me</h1>
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
+  );
+};
 
-
-
-    </div>
-  )
-}
-
-export default Dashboard
+export default Dashboard;
